6112a0f8f3c1e63cd055918e6cc0991c641ae841
Gavish.Manglore <gavishcm33@gmail.com>
2025-06-15 23:13:35 +0530

    Updated commit

--- PracticeDSA/InOrderTraversalBST.java
+++ PracticeDSA/InOrderTraversalBST.java
@@ -1,27 +1,17 @@
-
+import org.w3c.dom.Node;
 
 public class InOrderTraversalBST {
-
     Node root;
-
-    class Node {
-        Object data;
-        Node left;
-        Node right;
-        Node(Object data){
-            this.data=data;
-        }
-    }
-
-    void inOrder(Node node){
-        if(node!=null) {
-            
-        }
-    }
 
     public static void main(String[] args) {
         InOrderTraversalBST tree=new InOrderTraversalBST();
         tree.root=new Node(10);
+        tree.root.left=new Node(5);
+        tree.root.right=new Node(7);
+        tree.root.left.left=new Node(8);
+        tree.root.left.right=new Node(9);
+        tree.root.right.left=new Node(0);
+        tree.root.right.right=new Node(1);
 
     }
     static class Node{
@@ -32,5 +22,12 @@ public class InOrderTraversalBST {
             this.data=data;
         }
     }
-    
+    public void inOrder(Node root){
+        if (root!=null){
+            inOrder(root.left);
+            System.out.println(root.data);
+            inOrder(root.right);
+        }
+    }
+
 }
